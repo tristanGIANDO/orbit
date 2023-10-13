ddbbce3a8c04f2264dbb57857654e84777378cd8
Tristan Giandoriggio <83452758+tristanGIANDO@users.noreply.github.com>
2023-10-13 14:45:01 +0200

    Create envs.py

---
+++ envs.py
@@ -0,0 +1,6 @@
+GRAVITATIONAL_CONSTANT = 6.67430e-11 # Newton's gravitational constant
+
+SOLAR_MASS = 1.989e30 # Solar mass in kilograms
+
+MERCURY_D_FROM_SUN = 57910000000  #  in meters
+JUPITER_D_FROM_SUN = 778340000000
