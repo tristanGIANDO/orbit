5a3ad578c1cf511833dae2967eaeb5f6075ce500
Tristan Giandoriggio <83452758+tristanGIANDO@users.noreply.github.com>
2023-10-13 14:50:04 +0200

    Update get_revolution_time.py

--- get_revolution_time.py
+++ get_revolution_time.py
@@ -1,16 +1,16 @@
-mercury = 57910000000  #  in meters
-jupiter = 778340000000
+from general_physics import envs
 
-def get_revolution_time(semi_major_axis):
+def get_revolution_time(semi_major_axis:int) ->float:
+    """
+    Calculate the time in days that it takes for a star to make a complete revolution around the Sun from its distance from the Sun.
+    semi_major_axis : The semi-major axis of the ellipse formed by the star around the Sun
+    """
     # Using Kepler's law to calculate the orbital period (T)
-    gravitational_constant = 6.67430e-11  # Newton's gravitational constant
-    solar_mass = 1.989e30  # Solar mass in kilograms
-    
-    orbital_period = 2 * math.pi * math.sqrt((semi_major_axis**3) / (gravitational_constant * solar_mass))
+    orbital_period = 2 * math.pi * math.sqrt((semi_major_axis**3) / (envs.GRAVITATIONAL_CONSTANT * envs.SOLAR_MASS))
     
     # Conversion to days
     orbital_period_days = orbital_period / (60 * 60 * 24)
     
     return orbital_period_days
     
-print(get_revolution_time(jupiter))
+print(get_revolution_time(envs.JUPITER_SEMI_MAJOR_AXIS))
