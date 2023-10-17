3d64562e388b7c0e1f94dd8388069fdebfd102ff
tristanGIANDO <83452758+tristanGIANDO@users.noreply.github.com>
2023-10-17 20:17:07 +0200

    get revolution time

--- solar_system_cartography/api.py
+++ solar_system_cartography/api.py
@@ -15,6 +15,32 @@ def convert_meters_to_au(meters:int) ->float:
         raise
     return meters / envs.AU
 
+def convert_au_to_meters(au:float) ->float:
+    """Converts a distance in astronomical unit to meters
+
+    Args:
+        au (float): astronomical unit distance
+
+    Returns:
+        float: The same distance but in astronomical unit
+    """
+    if not au:
+        raise
+    return au * envs.AU
+
+def get_revolution_time(semi_major_axis:int) ->float:
+    """
+    Calculate the time in days that it takes for a star to make a complete revolution around the Sun from its distance from the Sun.
+    semi_major_axis : The semi-major axis of the ellipse formed by the star around the Sun
+    """
+    # Using Kepler's law to calculate the orbital period (T)
+    orbital_period = 2 * math.pi * math.sqrt((semi_major_axis**3) / (envs.GRAVITATIONAL_CONSTANT * envs.SOLAR_MASS))
+    
+    # Conversion to days
+    orbital_period_days = orbital_period / (60 * 60 * 24)
+    
+    return orbital_period_days
+
 def get_semi_minor_axis(semi_major_axis:float, eccentricity:float) ->float:
     """Calculates the semi minor axis of an ellipse.
 
@@ -43,22 +69,6 @@ def get_perihelion():
 
     return argument_du_perihelie_degrees
 
-# # Maintenant, vous pouvez utiliser l'argument du périhélie pour calculer l'orientation de l'axe semi-majeur comme précédemment.
-
-# # Les éléments orbitaux
-# inclinaison_degrees = 162.238  # Inclinaison de l'orbite en degrés
-# argument_du_perihelie_degrees = get_perihelion()
-# noeud_ascendant_degrees = 58
-
-# # Conversion des angles en radians
-# inclinaison_radians = math.radians(inclinaison_degrees)
-# argument_du_perihelie_radians = math.radians(argument_du_perihelie_degrees)
-# noeud_ascendant_radians = math.radians(noeud_ascendant_degrees)
-
-# # Calcul de l'orientation de l'axe semi-majeur par rapport à l'axe vertical Nord du Soleil
-# orientation_radians = argument_du_perihelie_radians + noeud_ascendant_radians
-# orientation_degrees = math.degrees(orientation_radians)
-
-# print("L'orientation de l'axe semi-majeur par rapport à l'axe vertical Nord du Soleil est de", orientation_degrees, "degrés.")
-
-print(get_semi_minor_axis(30.0699, 0.00859))
+if __name__ == "__main__":
+    # print(get_semi_minor_axis(30.0699, 0.00859))
+    print(get_revolution_time(convert_au_to_meters(0.38709808989279954)))
--- solar_system_cartography/envs.py
+++ solar_system_cartography/envs.py
@@ -3,7 +3,3 @@ AU = 149597870700 # meters
 GRAVITATIONAL_CONSTANT = 6.67430e-11 # Newton's gravitational constant
 
 SOLAR_MASS = 1.989e30 # Solar mass in kilograms
-
-# planets semi major axis
-MERCURY_SEMI_MAJOR_AXIS = 57910000000  #  in meters
-JUPITER_SEMI_MAJOR_AXIS = 778340000000
