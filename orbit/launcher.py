22b08d28a2f0380df6b4a1f3337018d6a8677b31
tristanGIANDO <83452758+tristanGIANDO@users.noreply.github.com>
2023-11-08 07:14:23 +0100

    remove wip nodes

--- orbit/launcher.py
+++ orbit/launcher.py
@@ -1,18 +1,4 @@
-import sys
-import types
-
-def reload_module(module, verbose=False):
-    if isinstance(module, types.ModuleType):
-        module=module.__name__
-    for mod in list(sys.modules.keys()):
-        if module not in mod:
-            continue
-        mod=sys.modules.pop(mod)
-        if mod and verbose:
-            print('reload %s'%mod)
-
 if __name__ == "__main__":
-    reload_module("orbit")
     from orbit.ui import run_ui
     
     run_ui()
--- orbit/usage.py
+++
@@ -1,9 +0,0 @@
-from orbit.backend import ObjectInOrbit
-from orbit.envs import SOLAR_MASS
-from orbit.presets import PRESETS
-
-mercury = PRESETS[0]
-
-obj = ObjectInOrbit(*mercury, SOLAR_MASS)
-
-print(obj)
